a1fe16429ae840dfb79ae27a311636b548a6cd2d
Tomasz Jeznach <tjeznach@rivosinc.com>
2023-08-02 15:34:52 -0700

    iommu/riscv: Expose hardware internals through debugfs
    A debugfs interface /sys/kernel/debug/iommu/riscv/*/regs is
    created to report hardware memory mapped registers state.
    
    Signed-off-by: Tomasz Jeznach <tjeznach@rivosinc.com>

--- drivers/iommu/riscv/iommu.c
+++ drivers/iommu/riscv/iommu.c
@@ -13,6 +13,7 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/compiler.h>
+#include <linux/debugfs.h>
 #include <linux/dma-mapping.h>
 #include <linux/init.h>
 #include <linux/iommu.h>
@@ -31,9 +32,71 @@ MODULE_LICENSE("GPL v2");
 /* Timeouts in [us] */
 #define RISCV_IOMMU_DDTP_TIMEOUT       50000
 
+static int riscv_iommu_attach_identity_domain(struct iommu_domain *domain,
+					      struct device *dev)
+{
+	/* Global pass-through already enabled, do nothing for now. */
+	return 0;
+}
+
+static struct iommu_domain riscv_iommu_identity_domain = {
+	.type = IOMMU_DOMAIN_IDENTITY,
+	.ops = &(const struct iommu_domain_ops) {
+		.attach_dev = riscv_iommu_attach_identity_domain,
+	}
+};
+
+static int riscv_iommu_device_domain_type(struct device *dev)
+{
+	return IOMMU_DOMAIN_IDENTITY;
+}
+
+static struct iommu_group *riscv_iommu_device_group(struct device *dev)
+{
+	if (dev_is_pci(dev))
+		return pci_device_group(dev);
+	return generic_device_group(dev);
+}
+
+static int riscv_iommu_of_xlate(struct device *dev, struct of_phandle_args *args)
+{
+	return iommu_fwspec_add_ids(dev, args->args, 1);
+}
+
+static struct iommu_device *riscv_iommu_probe_device(struct device *dev)
+{
+	struct riscv_iommu_device *iommu;
+	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
+
+	/* Early bus-scan exit, will retry. */
+	if (!fwspec || !fwspec->iommu_fwnode)
+		return ERR_PTR(-ENODEV);
+
+	/* Pending IOMMU driver initialization, will retry */
+	if (!fwspec->iommu_fwnode->dev)
+		return ERR_PTR(-ENODEV);
+
+	iommu = dev_get_drvdata(fwspec->iommu_fwnode->dev);
+	if (!iommu)
+		return ERR_PTR(-ENODEV);
+
+	return &iommu->iommu;
+}
+
+static const struct iommu_ops riscv_iommu_ops = {
+	.owner = THIS_MODULE,
+	.identity_domain = &riscv_iommu_identity_domain,
+	.def_domain_type = riscv_iommu_device_domain_type,
+	.device_group = riscv_iommu_device_group,
+	.probe_device = riscv_iommu_probe_device,
+	.of_xlate = riscv_iommu_of_xlate,
+};
+
 void riscv_iommu_remove(struct riscv_iommu_device *iommu)
 {
+	iommu_device_unregister(&iommu->iommu);
 	iommu_device_sysfs_remove(&iommu->iommu);
+	riscv_iommu_debugfs_remove(iommu);
 }
 
 static int riscv_iommu_init_check(struct riscv_iommu_device *iommu)
@@ -78,13 +141,22 @@ int riscv_iommu_init(struct riscv_iommu_device *iommu)
 	riscv_iommu_writeq(iommu, RISCV_IOMMU_REG_DDTP,
 			   FIELD_PREP(RISCV_IOMMU_DDTP_MODE, RISCV_IOMMU_DDTP_MODE_BARE));
 
+	riscv_iommu_debugfs_setup(iommu);
+
 	rc = iommu_device_sysfs_add(&iommu->iommu, NULL, NULL, "riscv-iommu@%s",
 				    dev_name(iommu->dev));
 	if (WARN(rc, "cannot register sysfs interface\n"))
 		goto err_sysfs;
 
+	rc = iommu_device_register(&iommu->iommu, &riscv_iommu_ops, iommu->dev);
+	if (WARN(rc, "cannot register iommu interface\n"))
+		goto err_device_register;
+
 	return 0;
 
+err_device_register:
+	iommu_device_sysfs_remove(&iommu->iommu);
 err_sysfs:
+	riscv_iommu_debugfs_remove(iommu);
 	return rc;
 }
